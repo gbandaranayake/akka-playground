035b743db4b866ec503f1ca679f3ab2a722b62d6
infamouspotato <gayanb@hsenidmobile.com>
2019-08-10 13:43:33 +0530

    added child failure supervision strategy to the parent example

--- src/main/kotlin/playground/akka/Hello.kt
+++ src/main/kotlin/playground/akka/Hello.kt
@@ -2,6 +2,8 @@ package playground.akka
 
 import akka.actor.*
 import akka.event.Logging
+import scala.Option
+import scala.PartialFunction
 import java.time.Duration
 
 fun main(args: Array<String>) {
@@ -13,7 +15,8 @@ fun runChildActorSpawning() {
     val printerProps = Props.create(Printer::class.java, ::Printer)
     val printer = actorSystem.actorOf(printerProps, "printer-actor")
     val morningGreeterActor = actorSystem.actorOf(Greeter.props("Good morning ", printer), "morning-greeter-actor")
-    val goodDayGreeterActor = actorSystem.actorOf(WelcomeDrinkGreeter.props("Good day to you ", printer), "day-greeter-actor")
+    val goodDayGreeterActor =
+        actorSystem.actorOf(WelcomeDrinkGreeter.props("Good day to you ", printer), "day-greeter-actor")
     morningGreeterActor.tell(Greeter.WhoToGreet("John"), ActorRef.noSender())
     goodDayGreeterActor.tell(WelcomeDrinkGreeter.WhoToServe("John"), ActorRef.noSender())
     goodDayGreeterActor.tell(WelcomeDrinkGreeter.WhoToServe("John"), ActorRef.noSender())
@@ -54,8 +57,8 @@ class WelcomeDrinkGreeter(private val message: String, private val printer: Acto
     override fun createReceive(): Receive {
         return receiveBuilder().match(WhoToServe::class.java) {
             context.actorSelection("/user/day-greeter-actor/waiter").resolveOne(Duration.ofMillis(500))
-                .thenAccept { ref -> ref.tell(Waiter.Serve(it.guest), self)}
-                .handle {t, u ->
+                .thenAccept { ref -> ref.tell(Waiter.Serve(it.guest), self) }
+                .handle { t, u ->
                     val cause = u.cause
                     if (cause is ActorNotFound) {
                         val props = Props.create(Waiter::class.java) { Waiter() }
@@ -66,6 +69,24 @@ class WelcomeDrinkGreeter(private val message: String, private val printer: Acto
                     }
                 }
         }.build().orElse(super.createReceive())
+    }
+
+    object DefaultDecider : PartialFunction<Throwable, SupervisorStrategy.Directive> {
+        override fun isDefinedAt(x: Throwable?): Boolean {
+            return true
+        }
+
+        override fun apply(v1: Throwable?): SupervisorStrategy.Directive {
+            return when (v1) {
+                is Waiter.FellDownException -> SupervisorStrategy.restart()
+                else -> SupervisorStrategy.escalate()
+            }
+        }
+    }
+
+    override fun supervisorStrategy(): SupervisorStrategy {
+        return OneForOneStrategy(DefaultDecider)
+            .withMaxNrOfRetries(10)
     }
 
     data class WhoToServe(val guest: String)
@@ -93,6 +114,7 @@ class Waiter : AbstractActor() {
             log.info("Here, take this glass of ice cold drink ${it.receiver} \\_/")
             servedGuests.add(it.receiver)
             self.tell(CollectGlass(it.receiver), ActorRef.noSender())
+            throw FellDownException()
         }.match(CollectGlass::class.java) {
             Thread.sleep(10000)
             log.info("Hope you enjoyed your drink ${it.glassHolder} <= \\_/")
@@ -100,6 +122,11 @@ class Waiter : AbstractActor() {
         }.build()
     }
 
+    override fun preRestart(reason: Throwable?, message: Option<Any>?) {
+        log.info("Aww, seems gotta start fresh. Change of clothes in order")
+    }
+
     data class Serve(val receiver: String)
     data class CollectGlass(val glassHolder: String)
+    class FellDownException : Exception()
 }
